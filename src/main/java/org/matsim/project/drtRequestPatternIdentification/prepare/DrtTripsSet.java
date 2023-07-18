70fe3293ad3f4dd92a32d84821c0b62baed94f6c
Kenzotong <709567093@qq.com>
2023-07-18 16:02:49 +0200

    using TripStructureUtils.Trip class to collect all drt trips

--- src/main/java/org/matsim/project/drtRequestPatternIdentification/prepare/DrtTripsSet.java
+++ src/main/java/org/matsim/project/drtRequestPatternIdentification/prepare/DrtTripsSet.java
@@ -7,20 +7,17 @@ import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.application.analysis.DefaultAnalysisMainModeIdentifier;
-import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
-import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.router.MainModeIdentifier;
 import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.project.drtRequestPatternIdentification.basicStructures.DrtDemand;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrtTripsSet {
-
-//    public static void main(String[] args){
+    //    public static void main(String[] args){
 //        String configPath = "D:\\Thesis\\drt-scenarios\\drt-scenarios\\New-York-Manhattan\\nyc-drt.config.xml";
 //        if (args.length != 0) {
 //            configPath = args[0];
@@ -29,28 +26,21 @@ public class DrtTripsSet {
 //        System.out.println(getDrtDemandsSet(config));
 //    }
 
-    public static List<DrtDemand> getDrtDemandsSet(Config config){
-        List<DrtDemand> drtDemandsSet = new ArrayList<>();
-
+    public static List<TripStructureUtils.Trip> getDrtTripsSet(Config config){
+        List<TripStructureUtils.Trip> drtTrips = new ArrayList<>();
 
         Scenario scenario = ScenarioUtils.loadScenario(config);
-        Network network = scenario.getNetwork();
         Population population = scenario.getPopulation();
-
         MainModeIdentifier mainModeIdentifier = new DefaultAnalysisMainModeIdentifier();
 
-        List<DrtDemand> demands = new ArrayList<>();
         for (Person person : population.getPersons().values()) {
             List<TripStructureUtils.Trip> trips = TripStructureUtils.getTrips(person.getSelectedPlan());
             for (TripStructureUtils.Trip trip : trips) {
                 if (mainModeIdentifier.identifyMainMode(trip.getTripElements()).equals(TransportMode.drt)) {
-                    double departureTime = trip.getOriginActivity().getEndTime().orElseThrow(RuntimeException::new);
-                    Link fromLink = network.getLinks().get(trip.getOriginActivity().getLinkId());
-                    Link toLink = network.getLinks().get(trip.getDestinationActivity().getLinkId());
-                    demands.add(new DrtDemand(person.getId().toString(), fromLink, toLink, departureTime));
+                    drtTrips.add(trip);
                 }
             }
         }
-        return demands;
+        return drtTrips;
     }
 }
